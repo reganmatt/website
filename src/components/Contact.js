6bdbbc479653b5e7a1927b6300331bdf6af94d0c
reganmatt <104975201+reganmatt@users.noreply.github.com>
2023-05-23 16:07:22 +0100

    bluey..

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1>CONTACT</h1>
       {/* Add content and components specific to the Home page */}
     </div>
   );
