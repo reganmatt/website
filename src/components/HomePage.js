4a1182b8c2738802d200760b5edf0e43434e5fce
reganmatt <104975201+reganmatt@users.noreply.github.com>
2023-05-23 17:08:25 +0100

    adding selfies..

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import selfie6 from "../img/selfie6.JPG";
 
 const HomePage = () => {
   return (
-    <section>
-      <img src="../../public/img/selfie.JPG" alt="Matt Regan" />
+    <section className="home-page">
+      <div className="image-container">
+        <img src={selfie6} alt="Matt Regan" className="profile-image" />
+      </div>
       <h1>Ello..</h1>
       <p>
         I'm a musician. I make music for{" "}
